9256846ce162e7b9d181fb4be9ac21829e66ba67
Saklain Ahmad <saklainhmd@gmail.com>
2023-11-27 09:42:43 +0530

    new user setup

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -20,13 +20,18 @@ public class SpringSecurityConfiguration {
 
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
+		UserDetails userDetails1 = CreateNewUser("in28Minutes", "dummy");
+		UserDetails userDetails2 = CreateNewUser("saqlain", "12345");
+
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+	}
+
+	private UserDetails CreateNewUser(String username, String password) {
 		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
 
-		UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder).username("in28Minutes")
-				.password("dummy").roles("USER", "ADMIN").build();
-
-		return new InMemoryUserDetailsManager(userDetails);
-
+		UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder).username(username).password(password)
+				.roles("USER", "ADMIN").build();
+		return userDetails;
 	}
 
 	@Bean
